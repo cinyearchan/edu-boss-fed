547c60bec23b51247fb53ff8ca2efee54f12ad5a
cinyearchan <cinyearchan@gmail.com>
2020-10-22 18:56:58 +0800

    菜单列表

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="menu">
-    <el-card>
-      <el-button @click="$router.push({ name: 'menu-create' })">添加菜单</el-button>
+    <el-card class="box-card">
+      <div class="clearfix" slot="header">
+        <el-button @click="$router.push({ name: 'menu-create' })">添加菜单</el-button>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { getAllMenus } from '@/services/menu'
 
 export default Vue.extend({
-  name: 'menu'
+  name: 'MenuIndex',
+  data () {
+    return {
+      menuList: []
+    }
+  },
+  methods: {
+    async loadAllMenus () {
+      const { data } = await getAllMenus()
+      console.log(data)
+    }
+  },
+  created () {
+    this.loadAllMenus()
+  }
 })
 </script>
 
